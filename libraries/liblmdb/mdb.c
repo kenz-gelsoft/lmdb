845490d968e824dafc0a8b3fc095c47e2b4525d4
KENZ <KENZ.gelsoft@gmail.com>
2024-08-08 01:54:17 +0900

    Fix lmdb build error on Haiku

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -123,7 +123,7 @@ typedef SSIZE_T	ssize_t;
 #include <resolv.h>	/* defines BYTE_ORDER on HPUX and Solaris */
 #endif
 
-#if defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__)
+#if defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__) || defined(__HAIKU__)
 # define MDB_USE_POSIX_SEM	1
 # define MDB_FDATASYNC		fsync
 #elif defined(__ANDROID__)
